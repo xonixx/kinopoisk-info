d41e3675deb07999562faad19d5a5cf6d7943fc5
xonix <xonix@6118b4d3-f947-bc39-0831-45c33e6c7814>
2010-07-01 20:38:41 +0000

    impr

--- imdb_info.py
+++ imdb_info.py
@@ -1,23 +1,67 @@
 
 import sys
-#from simplejson-2.1.1-py2.5-win32.egg import simplejson as j
+import os
+from os.path import split, join, isfile, isdir
+import traceback
 
-sys.path.insert(0, 'simplejson-2.1.1-py2.5-win32.egg')
+script_dir = os.path.realpath(os.path.dirname(sys.argv[0]))
+sys.path.append(join(script_dir,'simplejson-2.1.1-py2.5-win32.egg'))
 
 import simplejson as j
+
+import re
+
+
+IMDB, KINOPOISK = 'imdb', 'kinopoisk'
+TRASH = [
+	'ru','rus','en','eng',
+	'dvdrip', 'tvrip']
+
+def fail(s):
+	print s
+	sys.exit(1)
 
 def main(args):
 	#print args
 	
 	if len(args) == 0:
-		print 'Not enough args'
-		sys.exit(1)
+		fail('Not enough args')
 		
 	path = args[0];
 	print path
 	
+	do_search(path, IMDB)
+	
 	input()
 
+def do_search(path, site):
+	if (isfile(path)): 
+		keywords = make_keywords(path)
+	else:
+		fail('dir not supported yet')
+	
+def make_search_string(path):
+	dn, fn = split(path)
+	_, dn = split(dn)
+	
+	dn_clean = clean(dn)
+	
+	print dn
+	print 'cleaned:', dn_clean
+	print fn
+	
+def clean(q):
+	trash_re = '|'.join(r'\[%s\]' % t for t in TRASH)
+	q = re.sub('(?i)' + trash_re,'',q)
+	return q
+	
 
 if __name__=='__main__':
-	main(sys.argv[1:]);
+	try:
+		main(sys.argv[1:])
+	except:
+		exc_type, exc_value, exc_traceback = sys.exc_info()
+		traceback.print_exception(exc_type, exc_value, exc_traceback,file=sys.stdout)
+		input()
+
+	
